55c8776f3585483a34e4608ac4deb973c88a2269
Guntur-Indrasara <gunturindrasara11@gmail.com>
2024-11-11 10:34:02 +0700

    starter FE

---
+++ README.md
@@ -0,0 +1,25 @@
+## **`Informasi client`**
+
+### **_`Title: TPQ-Client`_**
+
+---
+
+### **`Fitur:`**
+
+- **1. Santri**
+- **2. pengajar**
+- **3. User**
+- **4. Kelas**
+- **5. Jilid**
+- **6. Nilai**
+- **7. Spp**
+- **8. Progress**
+- **9. Laporan**
+
+---
+
+### **`Kekurangan:`**
+
+- masih starter
+
+---
---
+++ src/main/java/com/tpq/clinet/ClinetApplication.java
@@ -0,0 +1,15 @@
+package com.tpq.clinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinetApplication.class, args);
+		System.out.println("\n Client Running......");
+
+	}
+
+}
